73ca727e6ad90f2948ecd8ce4fb461b8c79ef9b4
Nikitosik <96630344+luckyberezka@users.noreply.github.com>
2022-12-11 21:37:13 +0300

    Add files via upload

---
+++ HW_2/solution.py
@@ -0,0 +1,48 @@
+import argparse
+import subprocess
+import validators
+
+
+def mtu_get(host):
+    process = subprocess.run(
+        ['cat', '/proc/sys/net/ipv4/icmp_echo_ignore_all'],
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL
+    )
+    if process.stdout == 1:
+        print('ICMP ERROR!')
+        exit(1)
+
+    left_border = 0
+    right_border = 8973
+
+    while left_border != right_border and left_border < right_border - 1:
+        middle_value = (left_border + right_border) // 2
+        process = subprocess.run(
+            ["ping", host, '-M', 'do', '-s', str(middle_value), '-c', '5'],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL
+        )
+        if process.returncode == 0:
+            left_border = middle_value
+        elif process.returncode == 1:
+            right_border = middle_value
+        else:
+            print('PING ERROR!')
+            exit(1)
+    return left_border
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('host')
+    args = parser.parse_args()
+    host = args.host
+    if not validators.domain(host):
+        print('Invalid host!')
+        exit(1)
+    print('MTU is {}'.format(mtu_get(host) + 28))
+
+
+if __name__ == '__main__':
+    main()
